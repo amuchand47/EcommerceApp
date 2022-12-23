47c7adda05c65af7eaebdaee9a368172d33f0096
Mohammad Chand Alam <amuchand47@gmail.com>
2022-12-23 17:05:04 +0530

    Updated the style of card on hovering

--- src/component/Navbar.js
+++ src/component/Navbar.js
@@ -3,6 +3,7 @@ import React from "react";
 import { Link } from "react-router-dom";
 
 const Navbar = (props) =>{
+  
   return (
     <>
       <nav className="navbar navbar-expand-lg bg-light">
--- src/component/ProductItem.js
+++ src/component/ProductItem.js
@@ -1,12 +1,24 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
 const ProductItem = (props) => {
+  
+  // created an hook that show the shadow on hovering over the products
 
-  let { title, desc, category, image } = props;
+  const [style, setstyle] = useState(null);
 
+  const updateStyleEnter =() =>{
+    setstyle("shadow-lg p-3 mb-5 bg-body rounded")
+  } 
+
+  const updateStyleLeave =() =>{
+    setstyle("null")
+  } 
+
+ let { title, desc, category, image } = props;
+  
   return (
     <div className="my-3">
-      <div className="card">
+      <div className={`card ${style}`} onMouseEnter={updateStyleEnter} onMouseLeave={updateStyleLeave}>
         <span
           className="position-absolute top-0 translate-middle badge rounded-pill bg-success"
           style={{ left: "99%", zIndex: "1" }}
@@ -14,7 +26,7 @@ const ProductItem = (props) => {
           {category}
           <span className="visually-hidden">unread messages</span>
         </span>
-        <img src={image} className="card-img-top" alt="..." />
+        <img src={image} className="card-img-top" alt="..." height={400} width={100} />
         <div className="card-body">
           <h5 className="card-title">{title} </h5>
           <p className="card-text">{desc}...</p>
